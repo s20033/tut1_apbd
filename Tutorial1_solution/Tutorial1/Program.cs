0d4b64effefb7a569f7388c02d8b5470d1be30e5
s20033 <s20033@LAB204-13>
2020-03-06 11:48:22 +0100

    Debugged

--- Tutorial1_solution/Tutorial1/Program.cs
+++ Tutorial1_solution/Tutorial1/Program.cs
@@ -9,24 +9,37 @@ namespace Tutorial1
     {
         public static async Task Main(string[] args)
         {
-            var weburl = args[0];
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(weburl);
+            var weburl = args.Length > 0 ? args[0] : throw new ArgumentNullException();
 
-            if (response.IsSuccessStatusCode)
+            //  if (weburl == null) { throw new ArgumentException("Url cannot be null"); }
+
+            // var x = weburl ?? throw new ArgumentException("Website url is null !!");
+
+            try
             {
-                var htmlContent = await response.Content.ReadAsStringAsync();
 
-                var regex = new Regex("[a-z]+[a-z0-9-]*@[a-z0-9-]\\.[a-z]+", RegexOptions.IgnoreCase);
-                var emailAddress = regex.Matches(htmlContent);
-                foreach (var match in emailAddress)
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync(weburl);
+
+                if (response.IsSuccessStatusCode)
                 {
-                    Console.WriteLine(match.ToString());
+                    var htmlContent = await response.Content.ReadAsStringAsync();
+
+                    var regex = new Regex("[a-z]+[a-z0-9-]*@[a-z0-9-]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                    var emailAddress = regex.Matches(htmlContent);
+                    foreach (var match in emailAddress)
+                    {
+                        Console.WriteLine(match.ToString());
+                    }
                 }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error while downloading the page");
             }
 
 
-            Console.WriteLine("All Email from the Webpages are Listed by dev");
+            
         }
     }
 }
